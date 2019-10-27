4e0d869d7b9bf2205415c62b3f265d4a0d520281
danc <dangcheng@hotmail.com>
2019-10-27 21:45:24 +0800

    test

--- main/main.go
+++ main/main.go
@@ -6,11 +6,19 @@ import (
 	"github.com/danclive/snap7-go"
 )
 
-func main() {
-	snap7_client, err := snap7.ConnentTo("127.0.0.1", 0, 0)
-	if err != nil {
-		fmt.Println(err)
-	}
+// func main() {
+// 	snap7_client, err := snap7.ConnentTo("127.0.0.1", 0, 0)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	fmt.Println(snap7_client)
+// 	fmt.Println(snap7_client)
+// }
+
+func main() {
+	host := "116.85.47.167"
+	client, err := snap7.ConnentTo2(host, 0x0200, 0x0200, 0)
+
+	fmt.Println(client)
+	fmt.Println(err)
 }
